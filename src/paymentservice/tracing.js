5b371e9921aa7c7b607cba4ade7f300f1b42339a
Svetlana Brennan <50715937+svetlanabrennan@users.noreply.github.com>
2021-12-03 16:45:34 -0600

    Update src/paymentservice/tracing.js
    Co-authored-by: Alan West <3676547+alanwest@users.noreply.github.com>

--- src/paymentservice/tracing.js
+++ src/paymentservice/tracing.js
@@ -3,7 +3,7 @@
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
-const {CollectorTraceExporter} = require('@opentelemetry/exporter-collector-grpc');
+const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector-grpc');
 const { GrpcInstrumentation } = require('@opentelemetry/instrumentation-grpc');
 const { PinoInstrumentation } = require('@opentelemetry/instrumentation-pino');
 const { Resource } = require('@opentelemetry/resources');
