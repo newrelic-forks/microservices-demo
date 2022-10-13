3743ecf0d71a7f12a3ed1d0004230e2cfdf9bd7b
Svetlana Brennan <50715937+svetlanabrennan@users.noreply.github.com>
2022-10-13 15:22:40 -0500

    update otel-js packages for node services (#76)
    * update otel-js packages for node services
    
    * Remove deprecated otlp grpc exporter

--- src/paymentservice/tracing.js
+++ src/paymentservice/tracing.js
@@ -3,7 +3,7 @@
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { BatchSpanProcessor } = require('@opentelemetry/sdk-trace-base');
-const { CollectorTraceExporter } = require('@opentelemetry/exporter-collector-grpc');
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-grpc');
 const { GrpcInstrumentation } = require('@opentelemetry/instrumentation-grpc');
 const { PinoInstrumentation } = require('@opentelemetry/instrumentation-pino');
 const { Resource } = require('@opentelemetry/resources');
@@ -26,7 +26,7 @@ const collectorOptions = {
   url,
 };
 
-const traceExporter = new CollectorTraceExporter(collectorOptions);
+const traceExporter = new OTLPTraceExporter(collectorOptions);
 
 traceProvider.addSpanProcessor(new BatchSpanProcessor(traceExporter));
 
